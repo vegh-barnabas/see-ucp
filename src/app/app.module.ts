b4d6dd0b5d77576190acf5e4dc015074e0d770d0
vegh-barnabas <62122932+vegh-barnabas@users.noreply.github.com>
2025-10-16 00:04:43 +0100

    add login component and module, use it in app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginModule } from './components/login/login.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, LoginModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/components/login/login.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatCardModule } from '@angular/material/card';
+import { LoginComponent } from './login.component';
+
+@NgModule({
+  declarations: [LoginComponent],
+  imports: [CommonModule, MatCardModule],
+  exports: [LoginComponent],
+})
+export class LoginModule {}
