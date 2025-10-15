d8475bfe9a6578fd240c8a7507bc0f167672bc1e
vegh-barnabas <62122932+vegh-barnabas@users.noreply.github.com>
2025-10-16 00:18:06 +0100

    add login component card title and example fields. add buttons and some styling

--- src/app/components/login/login.module.ts
+++ src/app/components/login/login.module.ts
@@ -2,11 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 import { LoginComponent } from './login.component';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, MatCardModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   exports: [LoginComponent],
 })
 export class LoginModule {}
