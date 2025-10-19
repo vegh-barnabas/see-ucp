3d49e8ab162bc32c39bb48024348cf917662a713
vegh-barnabas <62122932+vegh-barnabas@users.noreply.github.com>
2025-10-19 12:39:02 +0100

    add forgot-password component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,17 @@ import { AppComponent } from './app.component';
 import { LoginModule } from './components/login/login.module';
 import { RegisterModule } from './components/register/register.module';
 import { NotFoundModule } from './components/not-found/not-found.module';
+import { ForgotPasswordModule } from './components/forgot-password/forgot-password.module';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 
 export const AppRoutes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: '**', component: NotFoundComponent, data: { errorCode: 404 } },
 ];
 
@@ -27,6 +30,7 @@ export const AppRoutes: Routes = [
     LoginModule,
     RegisterModule,
     NotFoundModule,
+    ForgotPasswordModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
