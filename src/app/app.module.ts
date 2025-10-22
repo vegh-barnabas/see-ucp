469e4686a80eac7e2e9ff473871ebb9ea48d4e9c
vegh-barnabas <62122932+vegh-barnabas@users.noreply.github.com>
2025-10-22 20:29:41 +0100

    put auth related components into auth folder. update imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { LoginModule } from './components/login/login.module';
-import { RegisterModule } from './components/register/register.module';
-import { NotFoundModule } from './components/not-found/not-found.module';
-import { ForgotPasswordModule } from './components/forgot-password/forgot-password.module';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { NotFoundComponent } from './components/not-found/not-found.component';
-import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { LoginModule } from './components/auth/login/login.module';
+import { RegisterModule } from './components/auth/register/register.module';
+import { NotFoundModule } from './components/auth/not-found/not-found.module';
+import { ForgotPasswordModule } from './components/auth/forgot-password/forgot-password.module';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { NotFoundComponent } from './components/auth/not-found/not-found.component';
+import { ForgotPasswordComponent } from './components/auth/forgot-password/forgot-password.component';
 
 export const AppRoutes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
