91b9cfe479cb15b2f7ae9715664bccd7e2768848
vegh-barnabas <62122932+vegh-barnabas@users.noreply.github.com>
2025-10-23 18:04:19 +0100

    client: create shared folder and add password match validator. Add path mapping to 'shared'.

---
+++ client/src/app/shared/password-match-validator.directive.ts
@@ -0,0 +1,10 @@
+import { AbstractControl, ValidationErrors, ValidatorFn, FormGroup } from '@angular/forms';
+
+export function passwordMatchValidator(passwordControlName: string): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    if (!(control.parent instanceof FormGroup)) return null;
+    const passwordControl = control.parent.get(passwordControlName);
+    if (!passwordControl) return null;
+    return control.value === passwordControl.value ? null : { passwordsMismatch: true };
+  };
+}
