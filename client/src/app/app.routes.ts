e42a70164e98cf0879c67ccbdfae843c63550a43
Végh Barnabás <62122932+vegh-barnabas@users.noreply.github.com>
2025-11-12 23:57:02 +0000

    client: add guard to redirect from auth pages if user already has a token.

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -5,12 +5,14 @@ import { RegisterComponent } from './components/auth/register/register.component
 import { NotFoundComponent } from './components/auth/not-found/not-found.component';
 import { ForgotPasswordComponent } from './components/auth/forgot-password/forgot-password.component';
 import { HomeComponent } from './components/home/home.component';
+
 import { AuthGuard } from './components/auth/auth.guard';
+import { LoginRedirectGuard } from './components/auth/login-redirect.guard';
 
 export const AppRoutes: Routes = [
   { path: '', component: HomeComponent, canActivate: [AuthGuard] },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'login', component: LoginComponent, canActivate: [LoginRedirectGuard] },
+  { path: 'register', component: RegisterComponent, canActivate: [LoginRedirectGuard] },
+  { path: 'forgot-password', component: ForgotPasswordComponent, canActivate: [LoginRedirectGuard] },
   { path: '**', component: NotFoundComponent, data: { errorCode: 404 } },
 ];
---
+++ client/src/app/components/auth/login-redirect.guard.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from './auth.service';
+
+@Injectable({ providedIn: 'root' })
+export class LoginRedirectGuard implements CanActivate {
+  constructor(private auth: AuthService, private router: Router) { }
+
+  canActivate(): boolean {
+    console.log(this.auth.getAccessToken());
+
+    if (this.auth.getAccessToken()) {
+      this.router.navigate(['/']);
+      return false;
+    }
+    return true;
+  }
+}
