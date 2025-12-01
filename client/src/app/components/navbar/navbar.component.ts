eab5656960663859ae7f46f30a080a3def0af503
Végh Barnabás <62122932+vegh-barnabas@users.noreply.github.com>
2025-12-01 18:59:24 +0000

    client: format navbar, add buttons, add logout

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 
-import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { AuthService } from '../auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [MatToolbarModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {}
+
+  public logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+}
